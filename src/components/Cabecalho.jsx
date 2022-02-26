1aac33b4876cb66d90cc73078a55893df96a8282
ERICK <erick_honorio20@hotmail.com>
2022-02-26 14:05:08 -0300

    terminando cabecalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -6,13 +6,19 @@ import ceta from "../assets/images/icon-arrow-down.svg"
 class Cabecalho extends Component {
     animacao(){
         const tela = document.querySelector('#tela')
+        const triangulo = document.querySelector('#triangulo')
         
         if(tela.classList.contains(('slide-in'))){
             tela.classList.remove('slide-in')
             tela.classList.add('slide-out')
+            triangulo.classList.remove('slide-in-t')
+            triangulo.classList.add('slide-out-t')
         }else{
             tela.classList.add('slide-in')
             tela.classList.remove('slide-out')
+            triangulo.classList.add('slide-in-t')
+            triangulo.classList.remove('slide-out-t')
+            
             
         }
     }
@@ -40,6 +46,7 @@ class Cabecalho extends Component {
                     <img src={ceta} alt="" className="cabecalho__ceta" />
                 </div>
                 <div className="tela" id='tela'>
+                    <span className="tela__triangulo" id='triangulo'></span>
                     <nav className="tela__menu">
                         <ul className="tela__menu__itens">
                             <li className="tela__menu__item"><a href="" className="tela__menu__link">About</a></li>

02bd5a7034aacb9bac6d15eed0aaf4371b301f78
ERICK <erick_honorio20@hotmail.com>
2022-02-26 17:40:45 -0300

    corpo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import "./assets/css/reset.css"
 import "./assets/css/style.css"
 import Cabecalho from './components/cabecalho';
+import Corpo from './components/corpo';
 
 class App extends Component {
 
    
   render() { 
     return (
-      <Cabecalho/>
+      <>
+        <Cabecalho/>
+        <Corpo/>
+      </>
     );
   }
 }

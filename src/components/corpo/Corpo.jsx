b10c041384bc9aed62f040729770c398863ae9b3
ERICK <erick_honorio20@hotmail.com>
2022-02-27 09:46:32 -0300

    responsivo

--- src/components/corpo/Corpo.jsx
+++ src/components/corpo/Corpo.jsx
@@ -5,12 +5,14 @@ class Corpo extends Component {
     render() { 
         return (
                 <section className="conteudo">
-                    <div className="conteudo__conteiner">
-                        <h2 className="conteudo__titulo">Transform your brand</h2>
-                        <p className="conteudo__texto">We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
-                        <a href="" className="conteudo__link link1">Learn more</a>
-                    </div>
+                    <div className="conteudo__conteiner-5">
+                        <div className="conteudo__conteiner">
+                            <h2 className="conteudo__titulo">Transform your brand</h2>
+                            <p className="conteudo__texto">We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+                            <a href="" className="conteudo__link link1">Learn more</a>
+                        </div>
                     <div className="conteudo__img img1"></div>
+                    </div>
                     <div className="conteudo__conteiner-2">
                         <div className="conteudo__conteiner">
                             <h2 className="conteudo__titulo">Stand out to the right audience</h2>
@@ -19,7 +21,6 @@ class Corpo extends Component {
                         </div>
                         <div className="conteudo__img img2"></div>
                     </div>
-                    <div>
                         <div className='conteudo__conteiner-3'>
                             <div className="conteudo__conteiner-4 img3">
                                 <h3 className="conteudo__titulo-2">Graphic design</h3>
@@ -30,7 +31,6 @@ class Corpo extends Component {
                                 <p className="conteudo__texto-2">Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
                             </div>
                         </div>
-                    </div>
                 </section>
         );
     }
